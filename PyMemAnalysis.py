748d5bd4bd5d951cc6ab28997f339b700f6189c9
ZWCmjw <majwemail@163.com>
2017-04-06 18:34:44 +0800

    2017.4.6.18.34

--- PyMemAnalysis.py
+++ PyMemAnalysis.py
@@ -1,3 +1,4 @@
+import  os,sys,linecache,codecs
 import tkinter as tk
 
 filemask = 'C:/Users/Administrator/Desktop/JavaCOS 空间修改器/c/mask.c'
@@ -16,6 +17,269 @@ def cur_file_dir():
 	elif os.path.isfile(path):
 		return os.path.dirname(path)
 
+# getjavaMSM(filecref,"0x10032000","0x10063A00")
+def getjavaMSM(file, rombase, e2base):
+	getROMBASE = None
+	getE2BASE = None
+	fline = open(file).readlines()
+	cnt = 0
+	for msm in fline:
+		if "ROMBASE=" in msm:
+			if rombase is not None:
+				fline[cnt] = "ROMBASE=" + rombase + "\n"
+			getROMBASE = fline[cnt]
+		elif "E2BASE=" in msm:
+			if e2base is not None:
+				fline[cnt] = "E2BASE=" + e2base + "\n"
+			getE2BASE = fline[cnt]
+		if getE2BASE and getROMBASE is not None:
+			break
+		cnt += 1
+
+	if rombase or e2base is not None:
+		fb = open(file, "w")
+		for l in fline:
+			fb.write(l)
+		fb.close()
+
+	return getROMBASE, getE2BASE
+
+
+# getcmaskMSM(filemask)
+def getcmaskMSM(file):
+	with codecs.open(file, 'r', 'gbk') as handle:
+		getROMSIZE = None
+		getPEEPROM_IMAGE_SIZE = None
+		for msm in handle:
+			# define ROMSIZE 175399
+			# define EEPROM_IMAGE_SIZE 12364
+			fileline = msm.replace(" ", "").replace("	", "").replace("\n", "").replace("\r", "")
+			if "#defineROMSIZE" in fileline:
+				getROMSIZE = fileline.replace("#defineROMSIZE","")
+			elif "#defineEEPROM_IMAGE_SIZE" in fileline:
+				getPEEPROM_IMAGE_SIZE = fileline.replace("#defineEEPROM_IMAGE_SIZE","")
+			if getROMSIZE and getPEEPROM_IMAGE_SIZE is not None:
+				break
+	handle.close()
+	return getROMSIZE, getPEEPROM_IMAGE_SIZE
+
+
+newsize1 = ["0x00", "0x09", "0xA7", "0x00"]
+
+
+def ModifycnvmMSM(file, newsize):
+	fline = open(file).readlines()
+	cnt = 0
+	for msm in fline:
+		fileline = msm
+		if "/*E2P_CONFIG_SIZE " in fileline:
+			index1 = fileline.upper().find("0X")
+			index2 = fileline.upper().find("0X", index1 + 1)
+			index3 = fileline.upper().find("0X", index2 + 1)
+			index4 = fileline.upper().find("0X", index3 + 1)
+			index5 = fileline.upper().find(",", index4 + 1)
+			if newsize is not None:
+				t = msm[:index1] + newsize[0] + "," + newsize[1] + "," + newsize[2] + "," + newsize[3] + msm[index5:]
+			else:
+				t = [fileline[index1:index2 - 1], fileline[index2:index3 - 1], fileline[index3:index4 - 1],
+					 fileline[index4:index5]]
+			break
+		cnt += 1
+
+	if newsize is not None:
+		fline[cnt] = t
+		fb = open(file, "w")
+		for l in fline:
+			fb.write(l)
+		fb.close()
+		return None
+	else:
+		return t
+
+
+def ALIGN_UP_OF(x, y):
+	return ((x + y - 1) & ~(y - 1))
+
+
+def getctarget(file, rombase, nvmbase, rambase, ramsize):
+	# flash起始地址   JC_EEPROM_BASE
+	# flash结束地址   EEPROM_LIMIT
+	# flash页大小     EEPAGE_SIZE
+	#
+	# ram 起始地址    JC_RAM_BASE
+	# ram 结束地址    RAM_LIMIT
+	#
+
+	# flash起始
+	# ROM_AREA_START            ----java层的 ROMBASE
+	# NVMBase                   ----平台起始地址
+	# OS_PARA_DATA_FIELD_START  ----平台配置区的起始地址
+	# OS_VARIABLE_FIELD_START   ----平台变量区的起始地址
+	# E2P_IMAGE_START           ----E2P表起始地址
+	# E2PBase                   ----用户可用的E2P起始地址
+	# EEP_BACKUP_AREA
+	# flash结束
+
+
+	# ram起始                   ----ram的起始地址
+	# DTR地址                   ----DTR起始地址以及大小
+	# RTR地址                   ----RTR起始地址以及大小
+	# ram结束                   ----ram的结束地址
+	returndata = []
+	fline = open(file).readlines()
+	cnt = 0
+	for msm in fline:
+		fileline = msm.replace(" ", "").replace("	", "").replace("\r", "").replace("\n", "").upper()
+
+		if "#DEFINENVMBASE" in fileline:
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			NVMBase = fileline[index0:index4].replace("UL", "")
+			if nvmbase is not None:
+				fline[cnt] = fline[cnt].replace(NVMBase[2:], nvmbase[2:])
+				NVMBase = int(nvmbase[2:], 16)
+			else:
+				NVMBase = int(NVMBase[2:], 16)
+		elif "#DEFINEROM_AREA_START" in fileline:  # rombase
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			ROMBase = fileline[index0:index4].replace("UL", "")
+			if rombase is not None:
+				fline[cnt] = fline[cnt].replace(ROMBase[2:], rombase[2:])
+				ROMBase = int(rombase[2:], 16)
+			else:
+				ROMBase = int(ROMBase[2:], 16)
+		elif "#DEFINERAMBASE" in fileline:  # RAMBase
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			RAMBase = fileline[index0:index4].replace("UL", "")
+			if rambase is not None:
+				fline[cnt] = fline[cnt].replace(RAMBase[2:], rambase[2:])
+				RAMBase = int(rambase[2:], 16)
+			else:
+				RAMBase = int(RAMBase[2:], 16)
+		elif "#DEFINERAMSIZE" in fileline:  # RAMSize
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 6
+			RAMSize = fileline[index0:index4].replace("UL", "")
+			if ramsize is not None:
+				fline[cnt] = fline[cnt].replace(RAMSize[2:], ramsize[2:])
+				RAMSize = int(ramsize[2:], 16)
+			else:
+				RAMSize = int(RAMSize[2:], 16)
+		elif "#DEFINEJC_RAM_BASE" in fileline:  # JC_RAM_BASE
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			JC_RAM_BASE = fileline[index0:index4].replace("UL", "")
+			JC_RAM_BASE = int(JC_RAM_BASE[2:], 16)
+		elif "#DEFINERAM_LIMIT" in fileline:  # RAM_LIMIT
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			RAM_LIMIT = fileline[index0:index4].replace("UL", "")
+			RAM_LIMIT = int(RAM_LIMIT[2:], 16)
+		elif "#DEFINEEEPAGE_SIZE0X" in fileline:  # 页大小
+			EEPAGE_SIZE = int(fileline[20:23], 16)
+		elif "#DEFINEBACKUP_BLOCK_NUM" in fileline:  # 备份页数 32
+			BACKUP_BLOCK_NUM = int(fileline[23:25], 10)
+		elif "#DEFINETF_ADDR_CELL_SZIE" in fileline:  # 6
+			TF_ADDR_CELL_SZIE = int(fileline[24:25], 10)
+		elif "#DEFINETF_CELL_SZIE" in fileline:  # 2
+			TF_CELL_SZIE = int(fileline[19:20], 10)
+		elif "#DEFINEOS_PARA_DATA_FIELD_SIZE" in fileline:  # 0x400+0x200 = 0x600
+			index = fileline.find("0X")
+			OS_PARA_DATA_FIELD_SIZE = int(fileline[index + 2:], 16)
+		elif "#DEFINEOS_VARIABLE_FIELD_SIZE" in fileline:  # 0xF00
+			index = fileline.find("0X")
+			OS_VARIABLE_FIELD_SIZE = int(fileline[index + 2:], 16)
+		elif "#DEFINEJC_EEPROM_BASE" in fileline:
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			JC_EEPROM_BASE = fileline[index0:index4].replace("UL", "")
+			JC_EEPROM_BASE = int(JC_EEPROM_BASE[2:], 16)
+		elif "#DEFINEEEPROM_LIMIT" in fileline:
+			index0 = fileline.find("0X")
+			index1 = fileline.find(")", index0)
+			if index1 != -1:
+				index4 = index1
+			else:
+				index4 = index0 + 10
+			EEPROM_LIMIT = fileline[index0:index4].replace("UL", "")
+			EEPROM_LIMIT = int(EEPROM_LIMIT[2:], 16)
+		elif "#DEFINEEFCFG_FIELD_SIZE" in fileline:
+			index = fileline.find("0X")
+			EFCFG_FIELD_SIZE = int(fileline[index + 2:], 16)
+		elif "#DEFINEEEP_BACKUP_AREA_SIZE" in fileline:
+			index = fileline.find("0X")
+			EEP_BACKUP_AREA_SIZE = int(fileline[index + 2:], 16)
+
+		cnt += 1
+
+	TF_SIZE = BACKUP_BLOCK_NUM * TF_CELL_SZIE
+	RECORD_BUFFER_SIZE = BACKUP_BLOCK_NUM * TF_ADDR_CELL_SZIE
+	RECORD_BUFFER_TOTAL_SIZE = RECORD_BUFFER_SIZE + TF_SIZE
+	TRANSACTION_BUFFER_SIZE = ((BACKUP_BLOCK_NUM + 1) * EEPAGE_SIZE + RECORD_BUFFER_TOTAL_SIZE)
+	RECORD_PAGE_START = NVMBase
+	TRANSACTION_BUFFER_START = RECORD_PAGE_START + ALIGN_UP_OF(RECORD_BUFFER_TOTAL_SIZE, EEPAGE_SIZE)
+	BACKUP_RECORD_PAGE_AREA = (TRANSACTION_BUFFER_START + (BACKUP_BLOCK_NUM + 2) * EEPAGE_SIZE)
+	OS_PARA_DATA_FIELD_START = (BACKUP_RECORD_PAGE_AREA + EEPAGE_SIZE * 2)
+	OS_VARIABLE_FIELD_START = OS_PARA_DATA_FIELD_START + OS_PARA_DATA_FIELD_SIZE
+	OS_CONFIG_SIZE = (ALIGN_UP_OF(RECORD_BUFFER_TOTAL_SIZE, EEPAGE_SIZE) + (
+	BACKUP_BLOCK_NUM + 2 + 2) * EEPAGE_SIZE + OS_PARA_DATA_FIELD_SIZE + OS_VARIABLE_FIELD_SIZE)
+	E2P_IMAGE_START = NVMBase + OS_CONFIG_SIZE
+	EECFG1_ADDR = EEPROM_LIMIT - EFCFG_FIELD_SIZE + 1
+	EEP_BACKUP_AREA = EECFG1_ADDR - EEP_BACKUP_AREA_SIZE
+	g_ConfigSize = EEP_BACKUP_AREA - OS_VARIABLE_FIELD_START
+	# E2PBase = E2P_IMAGE_START + EEPROM_IMAGE_SIZE
+	# print(hex(g_ConfigSize))
+	if rombase or nvmbase or rambase or ramsize is not None:
+		fb = open(file, "w")
+		for l in fline:
+			fb.write(l)
+		fb.close()
+
+	returndata = [g_ConfigSize,
+				  JC_EEPROM_BASE, EEPROM_LIMIT, EEPAGE_SIZE, ROMBase,
+				  NVMBase,OS_PARA_DATA_FIELD_START, OS_PARA_DATA_FIELD_SIZE, OS_VARIABLE_FIELD_START,
+				  OS_VARIABLE_FIELD_SIZE, E2P_IMAGE_START, EEP_BACKUP_AREA,EEP_BACKUP_AREA_SIZE,
+				  JC_RAM_BASE, RAM_LIMIT, RAMBase,RAMSize
+				  ]
+	return returndata
+
+
+# print(getctarget(filetarget,None,None,None,None))
+# print(getctarget(filetarget,"0X10032000","0X10060000","0x20003000","0X3C00"))
+# print(getctarget(filetarget,"0X10033000","0X10061000","0x20003300","0X3A00"))
+# a = getctarget(filetarget,None,None,None,None)
+# for s in a:
+# 	print(hex(s))
 
 
 class Application(tk.Frame):
@@ -23,8 +287,8 @@ class Application(tk.Frame):
 		super().__init__(master)
 		self.pack()
 		self.create_Mainwidgets()
-		self.create_Canvas()
-
+		self.create_Canvas(None)
+		self.Basic_infor(None,None)
 
 	def create_Mainwidgets(self):
 		self.ProPath = tk.Button(self)
@@ -37,207 +301,110 @@ class Application(tk.Frame):
 		self.Analysis["command"] = self.MemAnalysis
 		self.Analysis.grid(row=0, column=1, sticky='ew')
 
+		self.CheckMAP = tk.Button(self)
+		self.CheckMAP["text"] = "检查Map "
+		self.CheckMAP["command"] = self.CheckMap
+		self.CheckMAP.grid(row=0, column=2, sticky='ew')
+
+		self.TextMSM = tk.Label(self,text ="基本信息 " ,width=30,height=1,relief ='flat')
+		self.TextMSM.grid(row=0, column=3,columnspan = 4,sticky='ew')
+
 		self.Modify = tk.Button(self)
 		self.Modify["text"] = "  修改  "
 		self.Modify["command"] = self.MemModify
-		self.Modify.grid(row=0, column=2, sticky='ew')
-
-		self.Recovery = tk.Button(self)
-		self.Recovery["text"] = "  恢复  "
-		self.Recovery["command"] = self.Recovery
-		self.Recovery.grid(row=0, column=3, sticky='ew')
-
-		self.Submit = tk.Button(self)
-		self.Submit["text"] = "  提交  "
-		self.Submit["command"] = self.Submit
-		self.Submit.grid(row=0, column=4, sticky='ew')
-
-		self.CheckMAP = tk.Button(self)
-		self.CheckMAP["text"] = "检查Map "
-		self.CheckMAP["command"] = self.Submit
-		self.CheckMAP.grid(row=0, column=5, sticky='ew')
+		self.Modify.grid(row=8, column=6, sticky='ew')
 
 
-	def create_Addr(self):
-		Addr_text = tk.Text(self,width=10, height=50,bg="lightgrey")
-		Addr_text.grid(row=1, column=0, sticky='ew')
 
-	def create_Canvas(self):
+
+
+
+
+
+
+	def create_Canvas(self,data):
 		# flat, groove, raised, ridge, solid, or sunken  
-		cvs = tk.Canvas(self,width=60, height=600,bg = 'white',relief ='groove')
-		cvs.grid(row=1, column=0,columnspan=2, padx= 8,pady= 4, sticky='ew')
-		cvs.create_text(60,500,text = "12345")
-		# cvs.create_rectangle(0, 0, 147, 300, fill="gray")
-		# cvs.create_rectangle(200, 0,300, 50, fill="red")
+		cvs = tk.Canvas(self,width=200, height=600,bg = 'white',relief ='groove')
+		cvs.grid(row=1, column=0,rowspan=8 ,columnspan=3, padx= 8,pady= 4, sticky='ew')
+		if data is not None:
+			# cvs.create_text(60,500,text = "12345")
+			if data[1] > data[13]:
+				ramy1 = 600
+				ramy0 = 400
+				nvmy1 = 350
+				nvmy2 = 0
+			else:
+				nvmy1 = 600
+				nvmy2 = 250
+				ramy1 = 200
+				ramy0 = 0
+			cvs.create_text(100, nvmy - 5,  text="JC_EEPROM_BASE  " + hex(data[1]))
+			cvs.create_text(100, ramy + 45, text="EEPROM_LIMIT    " + hex(data[2]))
+			cvs.create_line(0,nvmy, 200, nvmy, fill='red')
+			cvs.create_line(0,ramy+50, 200, ramy+50, fill='red')
+			cvs.create_text(100, ramy - 5,  text="JC_RAM_BASE     " + hex(data[13]))
+			cvs.create_text(100,  10,       text="RAM_LIMIT       " + hex(data[14]))
+			cvs.create_line(0, ramy, 200, ramy, fill='red')
+			cvs.create_line(0, 15, 200, 15, fill='red')
+			# cvs.create_rectangle(0, 0, 200, 300, fill="grey") #white
+			# cvs.create_rectangle(200, 0,300, 50, fill="red")
+			# cvs.create_line(0, nvmy - 1, 200, nvmy - 1, fill='red', dash=(4, 4))  # 虚线
+
 
 
 	def SetProPath(self):
 		pass
 	def MemAnalysis(self):
-		pass
+		ROMSIZE, PEEPROM_IMAGE_SIZE = getcmaskMSM(filemask)
+		ROMBASE, E2BASE = getjavaMSM(filecref,None,None)
+		datalist = getctarget(filetarget,None,None,None,None)
+		t = ModifycnvmMSM(fileNvm,None)
+		ConfigSize = t[0][2:]+t[1][2:]+t[2][2:]+t[3][2:]
+		ConfigSize = int(ConfigSize,16)
+		datalist.append(int(ROMSIZE,10))
+		datalist.append(int(PEEPROM_IMAGE_SIZE,10))
+		datalist.append(ConfigSize)
+		for s in datalist:
+			print(hex(s))
+		self.Basic_infor(datalist,None)
+		self.create_Canvas(datalist)
+		# for s in datalist:
+		# 	print(hex(s))
+		# getctarget(filetarget)
 			
 	def MemModify(self):
 		pass
 			
 
-	def Recovery(self):
-		pass
-	def Submit(self):
-		pass
+	def CheckMap(self):
+		self.create_Canvas(1)
 
-
-# root = tk.Tk()
-# app = Application(master=root)
-# app.mainloop()
-
-
-# getjavaMSM(filecref,"0x10032000","0x10063A00")
-def getjavaMSM(file,rombase,e2base):
-	getROMBASE = None
-	getE2BASE  = None
-	fline = open(file).readlines()
-	cnt = 0
-	for msm in fline: 
-		if "ROMBASE=" in msm:
-			if rombase is not None:
-				fline[cnt] =  "ROMBASE=" + rombase + "\n"
-			getROMBASE = fline[cnt]
-		elif "E2BASE=" in msm:
-			if e2base is not None:
-				fline[cnt] =  "E2BASE=" + e2base + "\n"
-			getE2BASE  = fline[cnt]
-		if getE2BASE and getROMBASE is not None:
-			break
-		cnt+=1
-
-
-	if rombase or e2base is not None:	
-		fb = open(file,"w")
-		for l in fline:
-		    fb.write(l)
-		fb.close()
-
-	return getROMBASE, getE2BASE
-
-# getcmaskMSM(filemask)
-def getcmaskMSM(file):
-	with codecs.open(file, 'r', 'gbk') as handle:
-		getROMSIZE = None
-		getPEEPROM_IMAGE_SIZE  = None
-		for msm in handle: 
-		#define ROMSIZE 175399
-		#define EEPROM_IMAGE_SIZE 12364
-			fileline = msm.replace(" ","").replace("	","").replace("\n","").replace("\r","")
-			if "#defineROMSIZE" in fileline:
-				getROMSIZE = fileline
-				print(getROMSIZE)
-			elif "#defineEEPROM_IMAGE_SIZE" in fileline:
-				getPEEPROM_IMAGE_SIZE  = fileline
-				print(getPEEPROM_IMAGE_SIZE)
-			if getROMSIZE and getPEEPROM_IMAGE_SIZE is not None:
-				break
-	handle.close()
-	return 	getROMSIZE, getPEEPROM_IMAGE_SIZE
-
-newsize1 = ["0x00","0x09","0xA7","0x00"]
-def ModifycnvmMSM(file,newsize):
-	fline = open(file).readlines()
-	cnt = 0
-	for msm in fline: 
-		fileline = msm
-		if "/*E2P_CONFIG_SIZE " in fileline:
-			index1 = fileline.upper().find("0X")
-			index2 = fileline.upper().find("0X",index1+1)
-			index3 = fileline.upper().find("0X",index2+1)
-			index4 = fileline.upper().find("0X",index3+1)
-			index5 = fileline.upper().find(",",index4+1)
-			if newsize is not None:
-				t = msm[:index1] + newsize[0] +"," + newsize[1]  +"," + newsize[2]  +"," + newsize[3]  +msm[index5:]
+	def Basic_infor(self,data,map):
+		Basic_infor = tk.Text(self, width=20, height=15, bg="lightblue")
+		Basic_infor.grid(row=1, column=3,columnspan = 4, padx= 2,pady= 4, sticky='ewn')
+		if data is not None:
+			Basic_infor.insert(1.0, "NVM 页大小："+hex(data[3])+" （"+str(data[3])+"） " +"\n" )
+			Basic_infor.insert(2.0, "NVM 总空间：" + str(int((data[2] - data[1] +1)/1024)) +"K"+ "\n")
+			Basic_infor.insert(3.0, "RAM 总空间：" + str(int((data[14] - data[13] + 1) / 1024)) + "K" + "\n")
+			Basic_infor.insert(4.0, "JAVA占用NVM空间：" + str(int((data[17] + data[18]) / 1024)) + "K" + "\n")
+			Basic_infor.insert(5.0, "JAVA分配RAM空间：" + str(int((data[16]) / 1024)) + "K" + "\n")
+			Basic_infor.insert(6.0, "平台占用NVM空间：" + str(int((data[10] - data[5] + 1) / 1024)) + "K" + "\n")
+			if data[19] != data[0]:
+				Basic_infor.insert(7.0, "警告："+ "\n" )
+				Basic_infor.insert(8.0, "NvmDefaultData中的E2P_CONFIG_SIZE有误！"+"\n")
 			else:
-				t =[fileline[index1:index2-1], fileline[index2:index3-1], fileline[index3:index4-1], fileline[index4:index5]]
-			break 
-		cnt +=1
+				Basic_infor.insert(7.0, "\n")
+				Basic_infor.insert(8.0, "\n")
+		if map is not None:
+			Basic_infor.insert(9.0, "\n")
+			Basic_infor.insert(10.0, "     编译结果信息"  + "\n")
+			Basic_infor.insert(11.0, "\n")
+			Basic_infor.insert(12.0, "ROMBase前可以利用空间：" + "\n")
+			Basic_infor.insert(13.0, "警告："+ "\n" )
+			Basic_infor.insert(14.0, "平台起始地址与mask.c重叠!" + "\n")
 
-	if newsize is not None:	
-		fline[cnt] = t
-		fb = open(file,"w")
-		for l in fline:
-		    fb.write(l)
-		fb.close()
-		return None
-	else:
-		return t
-
-def getctarget(file,rombase,nvmbase, rambase,ramsize):
-#flash起始地址   JC_EEPROM_BASE
-#flash结束地址   EEPROM_LIMIT
-#flash页大小     EEPAGE_SIZE 
-# 
-#ram 起始地址    JC_RAM_BASE 
-#ram 结束地址    RAM_LIMIT 
-# 
-
-#flash起始
-#ROM_AREA_START            ----java层的 ROMBASE
-#NVMBase                   ----平台起始地址
-#OS_PARA_DATA_FIELD_START  ----平台配置区的起始地址
-#OS_VARIABLE_FIELD_START   ----平台变量区的起始地址
-#E2P_IMAGE_START           ----E2P表起始地址
-#E2PBase                   ----用户可用的E2P起始地址 
-# EEP_BACKUP_AREA 
-# flash结束
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
 
 
-#ram起始                   ----ram的起始地址
-#DTR地址                   ----DTR起始地址以及大小
-#RTR地址                   ----RTR起始地址以及大小
-#ram结束                   ----ram的结束地址
-	
-	fline = open(file).readlines()
-	cnt = 0
-	for msm in fline: 
-		fileline = msm.replace(" ","").replace("	","").upper()
-		
-		if "#DEFINENVMBASE" in fileline:
-			index0 = fileline.find("0X")
-			index1 = fileline.find(")",index0)
-			index2 = fileline.find("UL",index0)
-			index3 = fileline.find("U",index0)
-			index4 = min(index1,index2,index3)
-			print(index0,index1,index2,index3,index4)
-			# print(fileline[index:])
-		elif "#DEFINEEEPAGE_SIZE0X" in fileline:# 页大小
-			EEPAGE_SIZE = int(fileline[20:23],16)
-		elif "#DEFINEBACKUP_BLOCK_NUM"in fileline:# 备份页数 32
-			BACKUP_BLOCK_NUM = int(fileline[23:25],10)
-		elif "#DEFINETF_ADDR_CELL_SZIE"in fileline:# 6
-			TF_ADDR_CELL_SZIE = int(fileline[24:25],10)
-		elif "#DEFINETF_CELL_SZIE"in fileline:# 2
-			TF_CELL_SZIE = int(fileline[19:20],10) 
-
-
-
-		cnt +=1
-
-	TF_SIZE= BACKUP_BLOCK_NUM * TF_CELL_SZIE
-	RECORD_BUFFER_SIZE  = BACKUP_BLOCK_NUM * TF_ADDR_CELL_SZIE
-	RECORD_BUFFER_TOTAL_SIZE = RECORD_BUFFER_SIZE + TF_SIZE
-	TRANSACTION_BUFFER_SIZE = ((BACKUP_BLOCK_NUM + 1)  * EEPAGE_SIZE + RECORD_BUFFER_TOTAL_SIZE) 
-
-	# if RECORD_BUFFER_TOTAL_SIZE > EEPAGE_SIZE:
-	# 	TRANSACTION_BUFFER_START =  
-
-
-	# if newsize is not None:	
-	# 	fline[cnt] = t
-	# 	fb = open(file,"w")
-	# 	for l in fline:
-	# 	    fb.write(l)
-	# 	fb.close()
-	# 	return None
-	# else:
-	# 	return t
-	print(hex(TRANSACTION_BUFFER_SIZE))
-
-
-print(getctarget(filetarget,None,None,None,None))
